7aeec29c793d199a30878b08e9f28c8982158eb7
Taj Mace <taj.mac101@gmail.com>
2026-02-09 23:36:56 +1100

    accessable dashboard with blank submission file

--- __init__.py
+++ __init__.py
@@ -1,21 +1,19 @@
-from flask import render_template
+import os
+from flask import render_template, send_from_directory
 
 from CTFd.plugins import register_plugin_assets_directory
 
 def load(app):
 
-    # marking hub dashboard route (homepage)
+    # Custom asset route
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    assets_path = os.path.join(dir_path, "assets", "dist")
+    
+    @app.route("/marking_hub_assets/<path:filename>")
+    def marking_hub_assets(filename):
+        return send_from_directory(assets_path, filename)
+    
+    # marking hub dashboard route
     @app.route("/marking_hub", methods=["GET"])
     def marking_hub():
-        return render_template("dashboard.html")
-    
-    # submission viewing route
-
-    # mark table route
-
-    # plugin assets route
-    register_plugin_assets_directory(
-        app,
-        base_path="/plugins/CTFd_Marking_Hub/assets/",
-        admins_only=True
-    )
+        return render_template("plugins/CTFd_Marking_Hub/templates/marking_dashboard.html")
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,19 @@
 import './App.css'
 
+import SubmissionTile from '../components/dashboard/SubmissionTile'
+
 function App() {
   return (
-    <>
-      <div>
-        <h2>Marking Hub</h2>
-        <p>This is the marking dashboard</p>
-      </div>
-    </>
+    <div className="app-container">
+      <h1>1337 Marking Dashboard</h1>
+      <h2>Marking Hub</h2>
+      <SubmissionTile
+        name="Example Submission"
+        zid="z5691836"
+        submittedAt="2024-06-01 12:00:00"
+        mark={85}
+      />
+    </div>
   );
 }
 
